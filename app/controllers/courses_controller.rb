1df7c1282915a73b895339c126bcf5a55bf5b9b1
hyubooth <hyu80@uchicago.edu>
2025-05-13 00:31:55 +0000

    finished

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,21 +1,21 @@
 class CoursesController < ApplicationController
   def index
-    @courses = Course.all.order({ :created_at => :desc })
-
+    @course = Course.all.order({ :created_at => :desc })
+    
     render({ :template => "courses/index" })
   end
 
   def show
-    the_id = params.fetch("path_id")
-    @course = Course.where({:id => the_id }).at(0)
+    id = params.fetch("path_id")
+    @course = Course.where({:id => id }).at(0)
 
     render({ :template => "courses/show" })
   end
 
   def create
     @course = Course.new
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
+    @course.title = params.fetch("q_title")
+    @course.term_offered = params.fetch("query_term")
     @course.department_id = params.fetch("query_department_id")
 
     if @course.valid?
@@ -28,7 +28,7 @@ class CoursesController < ApplicationController
 
   def update
     id = params.fetch("path_id")
-    @course = Course.where({ :id => the_id }).at(0)
+    @course = Course.where({ :id => id }).at(0)
 
     @course.title = params.fetch("query_title")
     @course.term_offered = params.fetch("query_term_offered")
@@ -43,8 +43,8 @@ class CoursesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch("path")
-    @course = Course.where({ :id => the_id }).at(0)
+    id = params.fetch("path_id")
+    @course = Course.where({ :id => id }).at(0)
 
     @course.destroy
 
